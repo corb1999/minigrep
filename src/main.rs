d9362fcb8c011b04460e2c526a360d86764f7ed0
corb1999 <le.corbusier.29845@gmail.com>
2022-07-13 09:15:07 -0400

    super initial buildout basic args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// use std::env::args; // not needed to import
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n Hello, I am minigrep! \n");
+
+    let args: Vec<String> = std::env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2]; 
+
+    println!("{:?}", args);
+    println!("Searching for {}", query); 
+    println!("Within file {}", filename); 
+
 }
