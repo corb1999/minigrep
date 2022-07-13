17a8c44ba519f4d8bfb57ba07d4eb8b960addc99
corb1999 <le.corbusier.29845@gmail.com>
2022-07-13 11:00:19 -0400

    refactoring and adding error handling

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ fn main() {
 
     let args: Vec<String> = std::env::args().collect();
 
-    let config = Config::new(&args); 
+    let config = Config::new(&args)
+        .unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {}", err);
+            std::process::exit(1); 
+        }); 
 
     println!("{:?} \n", args);
     println!("Searching for... '{}' \n", config.query); 
@@ -24,15 +28,15 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("***Not enough arguments"); 
+            return Err("***Not enough arguments"); 
         }
 
         let query = args[1].clone(); 
         let filename = args[2].clone(); 
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
