6975e75f74d08e05bf419a586f27fd0db00afd09
corb1999 <le.corbusier.29845@gmail.com>
2022-07-13 09:47:35 -0400

    parse_config and Config struct refactor

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,27 @@ fn main() {
 
     let args: Vec<String> = std::env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2]; 
-
-    let contents = std::fs::read_to_string(filename)
-        .expect("Something went wrong trying to read the file"); 
+    let config = parse_config(&args); 
 
     println!("{:?}", args);
-    println!("Searching for... '{}'", query); 
-    println!("Within file... '{}'", filename); 
+    println!("Searching for... '{}'", config.query); 
+    println!("Within file... '{}'", config.filename);
+
+    let contents = std::fs::read_to_string(config.filename)
+        .expect("Something went wrong trying to read the file"); 
+
     println!("Which has text of... \n{}", contents);
 
+}
+
+struct Config {
+    query: String, 
+    filename: String, 
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone(); 
+
+    Config { query, filename }
 }
